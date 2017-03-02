da21a47e45447f2d15f611041ed96e0e61ffd306
dineshbabur92 <dineshbabur92@gmail.com>
2017-03-02 17:15:12 +0530

    week 2 exercise completed

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -16,8 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    for ca=1:m
-        theta = theta - (alpha * (1/m)) .* ((X*theta) - y) .* X
+    theta = theta - (alpha * (1/m)) .* (((X*theta) - y)' * X)'
 
 
 
